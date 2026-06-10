0eedcec749d711e801c1d1f87e63a2ff2706a9e7
Maurik24 <99667708+Maurik24@users.noreply.github.com>
2026-06-10 09:30:16 +0200

    oscillating drop with viscous damping Lamb test case setup

--- main_SH.jl
+++ main_SH.jl
@@ -1,26 +1,35 @@
 import IncompressibleNavierStokes as N
 include("src/time_stepping.jl")
 
-n = 16
-L = 1e-1
-ax = N.tanh_grid(0.0, L, n)
+n = 40
+# L = 1e-2
+L = 4.0
+# ax = N.tanh_grid(0.0, L, n)
+x = LinRange(0.0, L, n+1), LinRange(0.0, L, n+1), LinRange(0.0, L, n+1)
 setup = N.Setup(;
-    x = (ax, ax, ax),
+    x = x,
     boundary_conditions = (;
         u = (
-            (N.DirichletBC(), N.DirichletBC()),
-            (N.DirichletBC(), N.DirichletBC()),
-            (N.DirichletBC(), N.DirichletBC()),
+            (N.SymmetricBC(), N.SymmetricBC()),   # free-slip
+            (N.SymmetricBC(), N.SymmetricBC()),
+            (N.SymmetricBC(), N.SymmetricBC()),
+            # (N.DirichletBC(), N.DirichletBC()),   # no-slip
+            # (N.DirichletBC(), N.DirichletBC()),
+            # (N.DirichletBC(), N.DirichletBC()),
+            # (N.PeriodicBC(), N.PeriodicBC()),
+            # (N.PeriodicBC(), N.PeriodicBC()),
+            # (N.PeriodicBC(), N.PeriodicBC()),
         ),
     ),
 )
 
-psolver = N.default_psolver(setup)
+# psolver = N.default_psolver(setup)
 
-ncub, ℓₘ, R, σ = 8066, 10, 1e-3, 73e-3
+# ncub, ℓₘ, R, σ = 8066, 15, 1e-3, 73e-4
+ncub, ℓₘ, R, σ = 8066, 2, 1.0, 10.0
 Bub, Precomp_SH = bubble_setup(ncub, ℓₘ, R, σ, L)
 
 # Velocity field
 u = zeros(n + 2, n + 2, n + 2, 3);
 
-solveandplot(u, Bub, setup, psolver, Precomp_SH)
+solveandplot(u, Bub, setup, Precomp_SH, L);
